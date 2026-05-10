c464f1162904060676cd19156cf753270b4da689
Maurox <xpresar@gmail.com>
2026-05-10 02:01:15 -0500

    fix: add dashboard route

--- routes/reportes.routes.js
+++ routes/reportes.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { topSaboresController } from "../controllers/reportes.controller.js";
+import {
+  topSaboresController,
+  dashboardController,
+} from "../controllers/reportes.controller.js";
+import { verifyAuth, allowRoles } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/top-sabores", topSaboresController);
+
+router.get(
+  "/dashboard",
+  verifyAuth,
+  allowRoles("superadmin"),
+  dashboardController,
+);
 
 export default router;
