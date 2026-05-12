f8a6351aacbac353bc821610f72633bb427f55fb
Maurox <xpresar@gmail.com>
2026-05-12 18:14:56 -0500

    public descripcion

--- models/public.model.js
+++ models/public.model.js
@@ -8,6 +8,7 @@ export const getHeladosPublicos = async () => {
       h.precio,
       h.icon,
       h.fotos,
+      h.descripcion,
       h.cantidad,
       h.activo,
       h.id_categoria,
@@ -18,7 +19,8 @@ export const getHeladosPublicos = async () => {
       AND v.fecha >= NOW() - INTERVAL 7 DAY
       AND COALESCE(v.activo, 1) = 1
     WHERE h.activo = 1 AND h.cantidad > 0
-    GROUP BY h.id, h.sabor, h.precio, h.icon, h.fotos, h.cantidad, h.activo, h.id_categoria
+    GROUP BY h.id, h.sabor, h.precio, h.icon, h.fotos, h.descripcion,
+             h.cantidad, h.activo, h.id_categoria
     ORDER BY ventas_7dias DESC, nombre ASC;
   `);
 
